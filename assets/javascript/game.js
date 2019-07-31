4d98b708a83547b4ff6445564a422ec2c20b1287
Charles Danner <danner.ced1@gmail.com>
2019-07-31 01:47:20 -0400

    <error where the letter being selected by the computer was happening on every guess.>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,3 @@
-
-
 //array of letters computer can choose
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
@@ -7,6 +5,38 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
+
+
+
+
+function populateUnderScore(hangmanWord){
+    if (hangmanWord === "OUTLAW"){
+        randomWordText === ["_ " + "_ " + "_ " + "_ " + "_ " + "_ " + "_ " + "_"];
+        return 
+    }
+
+    if( hangmanWord ==="REVOLVER"){
+        randomWordText === ["_ " + "_ " + "_ " + "_ " + "_ " + "_ " + "_ " + "_"];
+        return
+    }
+
+    if (hangmanWord === "SALOON"){
+        randomWordText === ["_ " + "_ " + "_ " + "_ " + "_ " + "_"]
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 //empty array that will be populated for guesses user has made. will need to be populated and reset
 var guessesSoFar =[];
@@ -18,19 +48,27 @@ var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessesText = document.getElementById("guesses-made-text");
 
+
+var computerLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 //runs when user presses a key
+
 document.onkeyup = function(event){
     //computer randomly selects a number. number refers to the array with letters of alphabet to select a letter.
-    var computerLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    
     
     //user's guess is the key that is pressed
     var userGuess = event.key
 
+    function reset(){
+        guessesSoFar = []
+        guessesLeft = 9;
+        computerLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    }
+
     //if computer and user have same letter selected, user wins and wins are incremented.
     if (userGuess === computerLetter)
         {wins++;
-        guessesSoFar = [];
-        guessesLeft = 9;
+        reset()
     
     //if user guess does not equal computer guess, push guess into userGuess value into guessesSoFar array.
     // reduce the number of guesses by one.  
@@ -41,8 +79,8 @@ document.onkeyup = function(event){
         //when doing else if if the guessesLeft === 0 then add a loss, reset the guessesSoFar array and repopulate the guesses left number.
             if (guessesLeft === 0){
                 losses++;
-                guessesSoFar = [];
-                guessesLeft = 9;
+                reset()
+
             }
 
         }
@@ -59,6 +97,3 @@ document.onkeyup = function(event){
         
 
 }
-
-
-
